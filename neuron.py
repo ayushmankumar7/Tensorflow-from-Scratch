c4108bbdc9a57f8603fc4b94eea74ab76da9801f
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-07-20 22:29:26 +0530

    neuron class

---
+++ neuron.py
@@ -0,0 +1,24 @@
+from tensorflow import keras 
+import tensorflow as tf 
+
+
+class Neuron(keras.layers.Layer):
+
+    def __init__(self, units = 32, input_dim = 32):
+
+        super(Neuron, self).__init__()
+        w_init = tf.random.normal_initializer()
+        self.w = tf.Variable(
+            initial_value= w_init(shape = (input_dim, units), dtype = "float32"),
+            trainable= True
+        )
+
+        b_init = tf.zeros_initializer()
+        self.b = tf.Variable(
+            initial_value= b_init(
+                shape = (units,), 
+                dtype="float32"),
+            trainable = True
+        )
+
+    
