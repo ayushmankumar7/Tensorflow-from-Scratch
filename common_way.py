ceb049a9bf6b26b0ffc68766811d27113ab66704
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-07-20 23:00:44 +0530

    __call__

---
+++ common_way.py
@@ -0,0 +1,26 @@
+from tensorflow import keras 
+import tensorflow as tf 
+
+
+class Linear(keras.layers.Layer):
+    def __init__(self, units =32):
+
+        super(Linear, self).__init__()
+        self.units = units 
+    
+    def build(self, input_shape):
+
+        self.w = self.add_weight(
+            shape= (input_shape[-1], self.units),
+            initializer= "random_normal",
+            trainable= True
+        )
+
+        self.b = self.add_weight(
+            shape= (self.units, ),
+            initializer= "random_normal"
+            trainable= True
+        )
+
+    def call(self.inputs):
+        return tf.matmul(inputs, self.w) + self.b 
