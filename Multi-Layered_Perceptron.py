c589c2c6ac67f513a4870f5299270b1a38d56012
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-07-20 23:18:44 +0530

    MLP

---
+++ Multi-Layered_Perceptron.py
@@ -0,0 +1,29 @@
+from tensorflow import keras 
+import tensorflow as tf 
+
+# Calling the Linear Class from common_way.py
+from common_way import Linear
+
+class MLPBlock(keras.layers.Layer):
+
+    def __init__(self):
+
+        self.linear_1 = Linear(32)
+        self.linear_2 = Linear(32)
+        self.linear_3 = Linear(1)
+
+    def __call__(self, inputs):
+        
+        x = self.linear_1(inputs)
+        x = tf.nn.relu(x)
+        x = self.linear_2(x)
+        x =tf.nn.relu(x)
+        return self.linear_3(x)
+
+    
+
+mlp = MLPBlock()
+y = mlp(tf.ones(shape =(3,64)))
+
+print('Weights' , mlp.weights)
+print("trainable parameters", len(mlp.trainable_weights))
